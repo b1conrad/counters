eb388a82415b58057687b71c34101e9145b49189
Bruce Conrad <github@sanbachs.com>
2018-04-18 14:50:42 -0600

    add comments

--- fragment2KRL.bash
+++ fragment2KRL.bash
@@ -1,7 +1,7 @@
-for i in 0 1 2 3 4 5 6 7 8 9
+for i in 0 1 2 3 4 5 6 7 8 9              # for the ten digit fragments
 do
-  od -t u1 ~/sandbox/howto/count/$i |\
-  sed "s/^....... *//" |\
-  tr -s ' \n' ',' |\
-  sed "s/^\(.*\),$/    D$i = [\1];/"
+  od -t u1 ~/sandbox/howto/count/$i |\    # "octal dump" as decimal numbers
+  sed "s/^....... *//" |\                 # keep all but the line headers
+  tr -s ' \n' ',' |\                      # squeeze blanks and newlines into a single comma
+  sed "s/^\(.*\),$/    D$i = [\1];/"      # grab all but the final comma and put it between "    D0 = [" and "];"
 done
